0337c048a89627864beb528d13b760665fc2c34b
denrokokasdi <hpmsolutions.hq@gmail.com>
2024-12-11 22:41:09 +0800

    Update HappyPage.js

--- src/HappyPage.js
+++ src/HappyPage.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './HappyPage.css'; // Import the CSS file
 
 const HappyPage = () => {
+  const navigate = useNavigate(); // Initialize the navigate function
+
+  const handleBackClick = () => {
+    navigate('/'); // Navigate to the home page
+  };
+
   return (
     <div className="container">
       <h1>Nampaknya anda bahagia, teruskan bekerja</h1>
+      <button onClick={handleBackClick}>Back</button>
     </div>
   );
 };
